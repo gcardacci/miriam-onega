eaf214f325380b3e694d8e20e898c2d5d9016c55
Gabriel Cardacci <gabriel.cardacci@propago.com.ar>
2022-09-17 18:19:08 -0300

    [NextConfig] :: Images unoptimized

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	images: {
+		unoptimized: true
+	},
+	reactStrictMode: true,
+	swcMinify: true
 }
 
 module.exports = nextConfig
